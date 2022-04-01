f5c0218093e4815e82cecc153ae41cc26035d882
Code Sandbox <hello@codesandbox.io>
2022-04-01 17:02:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0be40e6ff4934659f478b54877fe55c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13b7607b15c76dcdc689af7f9e7dab2",
+    "url": "https://vulnexe.github.io/csb-9w0x52/index.html"
+  },
+  {
+    "revision": "276dff41874ca0ab757f",
+    "url": "https://vulnexe.github.io/csb-9w0x52/static/js/2.5f6b5108.chunk.js"
+  },
+  {
+    "revision": "c78b6282cda7b4b6a699",
+    "url": "https://vulnexe.github.io/csb-9w0x52/static/js/main.48d5bcf9.chunk.js"
+  },
+  {
+    "revision": "2f2798fcf635175ba440",
+    "url": "https://vulnexe.github.io/csb-9w0x52/static/js/runtime-main.04f51309.js"
+  }
+]);
